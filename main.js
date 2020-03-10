bb7138b1d7c746229ad22512d29fef5bdf49d86d
Javier Collao <jav.acollao@gmail.com>
2020-03-10 18:27:09 -0300

    adding js, and fixing css

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const body = document.querySelector('body')
 const responsive = window.matchMedia('screen and (max-width:959px)')
 const folderBtn = document.getElementById('folder')
 const sesionBtn = document.getElementById('sesion')
-
-
 folderBtn.addEventListener('click', mostrarfolder)
 function mostrarfolder() {
     if (tabFolder.classList.contains('is-active')) {
@@ -22,7 +20,6 @@ function mostrarfolder() {
         folderBtn.lastChild.classList.add('is-active')
     }
 }
-
 sesionBtn.addEventListener('click', mostrar)
 function mostrar() {
     if (tabSesion.classList.contains('is-active')) {
@@ -38,37 +35,28 @@ function mostrar() {
         sesionBtn.lastChild.classList.add('is-active')
     }
 }
-
 // Get the modal
 var modal = document.getElementById("myModal");
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
-
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
-
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-
-
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
